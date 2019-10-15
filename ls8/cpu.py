1667149a5e4412498b2e5dd1f76d24622a96793a
charlesgodoy <charlesgodoy@live.com>
2019-10-15 09:10:06 -0400

    making some corrections

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,11 +9,11 @@ class CPU:
     # Add list properties to the CPU class to hold 256 bytes of memory and 8 general-purpose registers.
     def __init__(self):
         """Construct a new CPU."""
-        self.pc
+        self.pc = 0     # added pc starting value here so just in case other methods need it
         self.ram = [0] * 256
-        self.register = [0] * 8
+        self.reg = [0] * 8
 
-    # Add RAM functions
+    # Add RAM functions 
     # ram_read() should accept the address to read and return the value stored there.
     # raw_write() should accept a value to write, and the address to write it to.
     def ram_read(self, ram_address):
@@ -77,14 +77,28 @@ class CPU:
     # perform the actions needed for the instruction per the LS-8 spec. Maybe an if-elif cascade...?
     def run(self):
         """Run the CPU."""
-        pc = 0  # program counter
         running = True
 
         HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
 
         while running:
             # Using ram_read(), read the bytes at PC+1 and PC+2 from RAM into variables operand_a and operand_b in case the instruction needs them.
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            
+            command = self.ram[self.pc]     # ram contains all instructions to execute, vales to read and write, etc and uses program counter to keep track of current index location
+
+            if command == HLT:  # ends program
+                running = False
+
+            elif command == LDI:
+                # int(self.reg[operand_a])
+                # self.pc += 1
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+
+            elif command == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
